f6e2033a929f23aba5b5c84c4229965992db6a30
Julian Lara <juiceinbox7@gmail.com>
2024-02-28 22:11:46 -0800

    added menu

---
+++ Assets/Scenes/MainMenu/Scripts/MenuScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] Button Play, Options, Quit;
+    [SerializeField] GameObject loadingScreen, configScreen, mainMenuCanvas, mainMenuScreen, optionsScreen, creditsScreen, saveSlotScreen;
+    
+    GameObject currentScreen;
+    List<GameObject> Dots = new List<GameObject>();
+    int curr = 0;
+
+    private void Start() {
+        currentScreen = loadingScreen;
+        GrabLoadingTextObj(loadingScreen.transform);
+        Invoke("SwitchToConfig", 5);
+    }
+
+    private void GrabLoadingTextObj(Transform loadingScreenTransform) {
+        foreach (Transform child in loadingScreenTransform) {
+            if (child.tag == "Loading") LoadingEllipsis(child.transform);
+        }
+    }
+
+    private void LoadingEllipsis(Transform loadingTextObj) {
+        int count = 1;
+        foreach (Transform child in loadingTextObj) {
+            Dots.Add(child.gameObject);
+            Invoke("setDotActive", count);
+            count++;
+        }
+    }
+
+    private void setDotActive() {
+        Dots[curr].SetActive(true);
+        curr++;
+    }
+
+    private void SwitchToConfig() {
+        currentScreen.SetActive(false);
+        mainMenuCanvas.SetActive(true);
+        currentScreen = configScreen;
+    }
+
+    public void OpenSaveScreen(){
+        currentScreen.SetActive(false);
+        saveSlotScreen.SetActive(true);
+        currentScreen = saveSlotScreen;
+    }
+
+    public void OpenOptions(){
+        currentScreen.SetActive(false);
+        optionsScreen.SetActive(true);
+        currentScreen = optionsScreen;
+    }
+
+    public void OpenCreditsScreen() {
+        currentScreen.SetActive(false);
+        creditsScreen.SetActive(true);
+        currentScreen = creditsScreen;
+    }
+
+    public void OpenMainMenu(){
+        currentScreen.SetActive(false);
+        mainMenuScreen.SetActive(true);
+        currentScreen = mainMenuScreen;
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+    
+
+}
