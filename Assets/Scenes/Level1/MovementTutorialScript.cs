8b3c8495849e1e893d2e026d8c56975809d2e7d2
jjam02 <35983461+jjam02@users.noreply.github.com>
2024-03-13 01:04:37 -0700

    movement tut txt changes correctly

--- Assets/Scenes/Level1/MovementTutorialScript.cs
+++ Assets/Scenes/Level1/MovementTutorialScript.cs
@@ -12,11 +12,14 @@ public class MovementTutorialScript : MonoBehaviour
     public Text backwards;
     public PlayerControllerPhysics player;
     float totalMouseMovement = 0;
+
+    private string state;
     private bool needLook, needMoveForward, needChangeSpeed, needMoveBackward, needJump;
 
     void Start()
     {
         Debug.Log(look.text);
+        state = "look";
         text.text = look.text;
         player.setMovementEnabled(false);
         needLook = needMoveForward = needChangeSpeed = needMoveBackward = needJump = true;
@@ -26,27 +29,53 @@ public class MovementTutorialScript : MonoBehaviour
     void Update()
     {
         totalMouseMovement += Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y"));
-        //Debug.Log(totalMouseMovement);
+
         if (needLook && totalMouseMovement > 100)
         {
             player.setMovementEnabled(true);
             text.text = move.text;
             needLook = false;
+            state = "move";
         }
         else if (!needLook && needMoveForward && Input.GetMouseButtonDown(1))
         {
             needMoveForward = false;
             text.text = scroll.text;
+            state = "scroll";
         }
         else if (!needMoveForward && needChangeSpeed && Mathf.Abs(Input.mouseScrollDelta.y) > 0)
         {
             needChangeSpeed = false;
             text.text = backwards.text;
+            state = "backwards";
         }
         else if (!needChangeSpeed && needMoveBackward && Input.GetMouseButtonDown(1) && player.getCurrentWalkingSpeed() < 0)
         {
             needMoveBackward = false;
             text.text = "";
+            state = "done";
+        }
+
+    }
+
+    public void refreshText()
+    {
+        switch (state)
+        {
+            case "look":
+                text.text = look.text;
+                break;
+            case "move":
+                text.text = move.text;
+                break;
+            case "scroll":
+                text.text = scroll.text;
+                break;
+            case "backwards":
+                text.text = backwards.text;
+                break;
+            default:
+                break;
         }
 
     }
